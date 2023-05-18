c1fdcc0987147f29b854012a7c0ec371e66d4e5e
Svitlana Synytsia <svetlankogr@gmail.com>
2023-05-18 17:49:20 +0200

    refs

--- src/components/CharList/CharList.jsx
+++ src/components/CharList/CharList.jsx
@@ -50,8 +50,22 @@ class CharList extends Component {
       error: true,
     });
   };
+
+  itemRefs = [];
+  setRef = (ref) => {
+    this.itemRefs.push(ref);
+  };
+
+  focusOnItem = (id) => {
+    this.itemRefs.forEach((item) =>
+      item.classList.remove("char__item_selected")
+    );
+    this.itemRefs[id].classList.add("char__item_selected");
+    this.itemRefs[id].focus();
+  };
+
   renderItems(array) {
-    const items = array.map((item) => {
+    const items = array.map((item, i) => {
       let imgStyle = { objectFit: "cover" };
       if (
         item.thumbnail ===
@@ -62,8 +76,19 @@ class CharList extends Component {
       return (
         <li
           className="char__item"
+          tabIndex={0}
+          ref={this.setRef}
           key={item.id}
-          onClick={() => this.props.onCharSelected(item.id)}
+          onClick={() => {
+            this.props.onCharSelected(item.id);
+            this.focusOnItem(i);
+          }}
+          onKeyPress={(e) => {
+            if (e.key === " " || e.key === "Enter") {
+              this.props.onCharSelected(item.id);
+              this.focusOnItem(i);
+            }
+          }}
         >
           <img src={item.thumbnail} alt={item.name} style={imgStyle} />
           <div className="char__name">{item.name}</div>
